59f81152427e0b348bf32198e7601326a7f589ea
Tim Crisp <timothycrisp@gmail.com>
2022-10-10 15:49:03 -0500

    add y-axis

--- section-4-1-STARTER/app.js
+++ section-4-1-STARTER/app.js
@@ -39,7 +39,7 @@ async function draw() {
     
     const yScale = d3.scaleLinear()
     .domain(d3.extent(dataset, yAccessor))
-    .rangeRound([0, dimensions.ctrHeight])
+    .rangeRound([dimensions.ctrHeight, 0])
     .nice()
     .clamp(true)
 
@@ -51,14 +51,36 @@ async function draw() {
     .attr('cx', d => xScale(xAccessor(d)))    
     .attr('cy', d => yScale(yAccessor(d)))
     .attr('r', 5)
-    .attr('fill', 'red') 
+    .attr('fill', 'red')
+    .attr('data-temp', yAccessor) 
 
   // Axes
   const xAxis = d3.axisBottom(xScale)
 
-  ctr.append('g')
+  const xAxisGroup = ctr.append('g')
     .call(xAxis)
     .style('transform', `translateY(${dimensions.ctrHeight}px)`)
+    .classed('axis', true)
+
+  xAxisGroup.append('text')
+    .attr('x', dimensions.ctrWidth / 2)
+    .attr('y', dimensions.margin.bottom - 10)
+    .attr('fill', 'black')
+    .text('Humidity')
+  
+  const yAxis = d3.axisLeft(yScale)
+
+  const yAxisGroup = ctr.append('g')
+    .call(yAxis)
+    .classed('axis', true)
+
+  yAxisGroup.append('text')
+    .attr('x', -dimensions.ctrHeight / 2)
+    .attr('y', -dimensions.margin.left + 15)
+    .attr('fill', 'black')
+    .html('Temperature &deg; F')
+    .style('transform', 'rotate(270deg)')
+    .style('text-anchor', 'middle')
 
     
 }
