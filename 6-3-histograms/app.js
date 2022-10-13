fade0129d7f82d37604e8829ab17ca49dc67da59
Tim Crisp <timothycrisp@gmail.com>
2022-10-13 15:17:08 -0500

    add mean to histogram

--- 6-3-histograms/app.js
+++ 6-3-histograms/app.js
@@ -31,6 +31,9 @@ async function draw() {
   const xAxisGroup = ctr.append('g')
     .style('transform', `translateY(${dimensions.ctrHeight}px)`)
 
+  const meanLine = ctr.append('line')
+      .classed('mean-line', true)
+
   function histogram(metric) {
     const xAccessor = d => d.currently[metric]
     const yAccessor = d => d.length
@@ -97,6 +100,15 @@ async function draw() {
       .attr('x', d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
       .attr('y', d => yScale(yAccessor(d)) - 10)
       .text(yAccessor)
+    
+    const mean = d3.mean(dataset, xAccessor)
+
+    meanLine.raise()
+        .transition(updateTransition)
+        .attr('x1', xScale(mean))
+        .attr('y1', 0)
+        .attr('x2', xScale(mean))
+        .attr('y2', dimensions.ctrHeight)
 
     // draw axis
     const xAxis = d3.axisBottom(xScale)
