dbe02482cd0fb4cde539aea54ba80c8f85a7a0c7
Tim Crisp <timothycrisp@gmail.com>
2022-10-13 15:10:53 -0500

    add transitions to histogram labels

--- 6-3-histograms/app.js
+++ 6-3-histograms/app.js
@@ -66,13 +66,13 @@ async function draw() {
           .attr('height', 0)
           .attr('x', d => xScale(d.x0))
           .attr('y', dimensions.ctrHeight)
-          .attr('fill', '#01c5c4'),
+          .attr('fill', '#b8de6f'),
         (update) => update,
-        (exit) => exit.transition(exitTransition)
+        (exit) => exit.attr('fill', '#f39233')
+          .transition(exitTransition)
           .attr('y', dimensions.ctrHeight)
           .attr('height', 0)
           .remove()
-
       )
       .transition(updateTransition)
       .attr('width', d => d3.max([0, xScale(d.x1) - xScale(d.x0) - padding]))
@@ -83,8 +83,17 @@ async function draw() {
 
     labelsGroup.selectAll('text')
       .data(newDataset)
-      .join('text')
-      .transition()
+      .join(
+        (enter) => enter.append('text')
+          .attr('x', d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
+          .attr('y', dimensions.ctrHeight)
+          .text(yAccessor),
+        (update) => update,
+        (exit) => exit.transition(exitTransition)
+          .attr('y', dimensions.ctrHeight)
+          .remove()
+      )
+      .transition(updateTransition)
       .attr('x', d => xScale(d.x0) + (xScale(d.x1) - xScale(d.x0)) / 2)
       .attr('y', d => yScale(yAccessor(d)) - 10)
       .text(yAccessor)
