9fe969992eef5803d6894c61fa1bba70fd7f3ff7
Tim Crisp <timothycrisp@gmail.com>
2022-10-25 12:03:56 -0500

    draw pie chart

---
+++ 7-10-pie-charts/app.js
@@ -0,0 +1,51 @@
+async function draw() {
+  // Data
+  const dataset = await d3.csv('data.csv')
+
+  // Dimensions
+  let dimensions = {
+    width: 600,
+    height: 600,
+    margins: 10,
+  };
+
+  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
+  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2
+  const radius = dimensions.ctrWidth / 2
+
+  // Draw Image
+  const svg = d3.select('#chart')
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height)
+
+  const ctr = svg.append("g") // <g>
+    .attr(
+      "transform",
+      `translate(${dimensions.margins}, ${dimensions.margins})`
+    )
+
+  // Scales
+  const populationPie = d3.pie()
+    .value((d) => d.value)
+
+  const slices = populationPie(dataset)
+  
+  const arc = d3.arc()
+    .outerRadius(radius)
+    .innerRadius(0)
+
+  // Draw Shape
+  const arcGroup = ctr.append('g')
+    .attr(
+      'transform', 
+      `translate(${dimensions.ctrHeight / 2}, ${dimensions.ctrWidth / 2})`
+    )
+  
+    arcGroup.selectAll('path')
+      .data(slices)
+      .join('path')
+      .attr('d', arc)
+}
+
+draw()
